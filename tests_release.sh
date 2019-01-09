b805268e3601133f61b87593e463564270164f14
sguada <sguada@google.com>
2019-01-08 19:15:41 -0800

    Allow testing nightly release in test_release.sh
    PiperOrigin-RevId: 228441792

--- tests_release.sh
+++ tests_release.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+# Test nightly release: ./test_release.sh nightly
+# Test stable release: ./test_release.sh stable
+
 # Exit if any process returns non-zero status.
 set -e
 # Display the commands being run in logs, which are replicated to sponge.
 set -x
 
+if [[ $# -lt 1 ]] ; then
+  echo "Usage:"
+  echo "test_release [nightly|stable]"
+  exit 1
+fi
+
 run_tests() {
+  echo "run_tests $1 $2"
   TMP=$(mktemp -d)
   # Create and activate a virtualenv to specify python version and test in
   # isolated environment. Note that we don't actually have to cd'ed into a
@@ -15,16 +25,31 @@ run_tests() {
   virtualenv -p "$1" "${VENV_PATH}"
   source ${VENV_PATH}/bin/activate
 
+
   # TensorFlow isn't a regular dependency because there are many different pip
   # packages a user might have installed.
-  pip install tensorflow
+  if [[ $2 == "nightly" ]] ; then
+    pip install tf-nightly
 
-  # Run the tests
-  python setup.py test --release
+    # Run the tests
+    python setup.py test
 
-  # Install tf_agents package.
-  WHEEL_PATH=${TMP}/wheel/$1
-  ./pip_pkg.sh ${WHEEL_PATH}/ --release
+    # Install tf_agents package.
+    WHEEL_PATH=${TMP}/wheel/$1
+    ./pip_pkg.sh ${WHEEL_PATH}/
+  elif [[ $2 == "stable" ]] ; then
+    pip install tensorflow
+
+    # Run the tests
+    python setup.py test --release
+
+    # Install tf_agents package.
+    WHEEL_PATH=${TMP}/wheel/$1
+    ./pip_pkg.sh ${WHEEL_PATH}/ --release
+  else
+    echo "Error unknow option only [nightly|stable]"
+    exit
+  fi
 
   pip install ${WHEEL_PATH}/tf_agents_*.whl
 
@@ -43,7 +68,9 @@ if ! which cmake > /dev/null; then
       sudo apt-get install -y cmake zlib1g-dev
    fi
 fi
+
 # Test on Python2.7
-run_tests "python2.7"
+run_tests "python2.7" $1
 # Test on Python3.5
-run_tests "python3.5"
+run_tests "python3.5" $1
+
