Fix a bug in py_tf_policy_test.py where arguments were being passed in the wrong order.
PiperOrigin-RevId: 236967661
Change-Id: I1ba6ce176a310514be0cf4e219c2b4860984d7bc

tf_agents/policies/py_tf_policy_test.py
@@ -44,7 +44,9 @@ class DummyNet(network.Network):
       state_spec = tensor_spec.TensorSpec(shape=(1,), dtype=tf.float32)
     else:
       state_spec = ()
-    super(DummyNet, self).__init__(name, state_spec, None)
+    super(DummyNet, self).__init__(input_tensor_spec=None,
+                                   state_spec=state_spec,
+                                   name=name)
 
     kernel_initializer = None
     bias_initializer = None