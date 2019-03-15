ac1578f1ea2a932115d9b3ce6a490152a5e764da
Youssef <y.bendhieb@instadeep.com>
2019-03-15 11:01:04 +0100

    Fix a typo

--- tf_agents/networks/dueling_q_network_test.py
+++ tf_agents/networks/dueling_q_network_test.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests for tf_agents.network.dueling_q_network."""
+"""Tests for tf_agents.networks.dueling_q_network."""
 
 from __future__ import absolute_import
 from __future__ import division
