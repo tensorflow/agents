e791394f54877f3bdfd5daa8fc7223955e68e312
Youssef <y.bendhieb@instadeep.com>
2019-03-15 10:51:52 +0100

    Add Dueling DQN

---
+++ tf_agents/networks/dueling_q_network.py
@@ -0,0 +1,139 @@
+# coding=utf-8
+# Copyright 2018 The TF-Agents Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Simple Keras Dueling DQNetwork."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import gin.tf
+
+from tf_agents.networks import encoding_network
+from tf_agents.networks import network
+
+
+
+
+def validate_specs(action_spec, observation_spec):
+  """Validates the spec contains a single action."""
+  del observation_spec  # not currently validated
+
+  flat_action_spec = tf.nest.flatten(action_spec)
+  if len(flat_action_spec) > 1:
+    raise ValueError('Network only supports action_specs with a single action.')
+
+  if flat_action_spec[0].shape not in [(), (1,)]:
+    raise ValueError(
+        'Network only supports action_specs with shape in [(), (1,)])')
+
+
+@gin.configurable
+class DuelingQNetwork(network.Network):
+  """Feed Forward network."""
+
+  def __init__(self,
+               input_tensor_spec,
+               action_spec,
+               preprocessing_layers=None,
+               preprocessing_combiner=None,
+               conv_layer_params=None,
+               fc_layer_params=(75, 40),
+               activation_fn=tf.keras.activations.relu,
+               kernel_initializer=None,
+               batch_squash=True,
+               dtype=tf.float32,
+               name='DuelingQNetwork'):
+    """Creates an instance of `DuelingQNetwork`.
+    Args:
+      input_tensor_spec: A nest of `tensor_spec.TensorSpec` representing the
+        input observations.
+      action_spec: A nest of `tensor_spec.BoundedTensorSpec` representing the
+        actions.
+      preprocessing_layers: (Optional.) A nest of `tf.keras.layers.Layer`
+        representing preprocessing for the different observations.
+        All of these layers must not be already built. For more details see
+        the documentation of `networks.EncodingNetwork`.
+      preprocessing_combiner: (Optional.) A keras layer that takes a flat list
+        of tensors and combines them. Good options include
+        `tf.keras.layers.Add` and `tf.keras.layers.Concatenate(axis=-1)`.
+        This layer must not be already built. For more details see
+        the documentation of `networks.EncodingNetwork`.
+      conv_layer_params: Optional list of convolution layers parameters, where
+        each item is a length-three tuple indicating (filters, kernel_size,
+        stride).
+      fc_layer_params: Optional list of fully_connected parameters, where each
+        item is the number of units in the layer.
+      activation_fn: Activation function, e.g. tf.keras.activations.relu.
+      kernel_initializer: Initializer to use for the kernels of the conv and
+        dense layers. If none is provided a default variance_scaling_initializer
+      batch_squash: If True the outer_ranks of the observation are squashed into
+        the batch dimension. This allow encoding networks to be used with
+        observations with shape [BxTx...].
+      dtype: The dtype to use by the convolution and fully connected layers.
+      name: A string representing name of the network.
+    Raises:
+      ValueError: If `input_tensor_spec` contains more than one observation. Or
+        if `action_spec` contains more than one action.
+    """
+    validate_specs(action_spec, input_tensor_spec)
+    action_spec = tf.nest.flatten(action_spec)[0]
+    num_actions = action_spec.maximum - action_spec.minimum + 1
+
+    encoder = encoding_network.EncodingNetwork(
+        input_tensor_spec,
+        preprocessing_layers=preprocessing_layers,
+        preprocessing_combiner=preprocessing_combiner,
+        conv_layer_params=conv_layer_params,
+        fc_layer_params=fc_layer_params,
+        activation_fn=activation_fn,
+        kernel_initializer=kernel_initializer,
+        batch_squash=batch_squash,
+        dtype=dtype)
+
+    fc_value_layer = tf.keras.layers.Dense(
+        1,
+        activation=None,
+        kernel_initializer=tf.compat.v1.initializers.random_uniform(
+            minval=-0.03, maxval=0.03),
+        bias_initializer=tf.compat.v1.initializers.constant(-0.2),
+        dtype=dtype)
+
+    fc_advantage_layer = tf.keras.layers.Dense(
+        num_actions,
+        activation=None,
+        kernel_initializer=tf.compat.v1.initializers.random_uniform(
+            minval=-0.03, maxval=0.03),
+        bias_initializer=tf.compat.v1.initializers.constant(-0.2),
+        dtype=dtype)
+
+    super(DuelingQNetwork, self).__init__(
+        input_tensor_spec=input_tensor_spec,
+        state_spec=(),
+        name=name)
+
+    self._encoder = encoder
+    self._fc_value_layer = fc_value_layer
+    self._fc_advantage_layer = fc_advantage_layer
+
+  def call(self, observation, step_type=None, network_state=()):
+    state, network_state = self._encoder(
+        observation, step_type=step_type, network_state=network_state)
+    value = self._fc_value_layer(state)
+    advantage = self._fc_advantage_layer(state)
+    q_value = tf.add(value, tf.subtract(
+        advantage, tf.reduce_mean(advantage, axis=1, keep_dims=True)))
+    return q_value, network_state
