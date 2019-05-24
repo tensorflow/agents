fe080ad28b3177ee1a861ba86bedababfc800ff7
Aurelien Geron <aurelien.geron@gmail.com>
2019-05-24 11:35:56 +0800

    Use tf.image.resize() rather than cv2, and extend gym.core.Wrapper

--- tf_agents/environments/atari_preprocessing.py
+++ tf_agents/environments/atari_preprocessing.py
@@ -30,14 +30,15 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from gym.core import Wrapper
 from gym.spaces.box import Box
 import numpy as np
 import gin.tf
-import cv2
+import tensorflow as tf
 
 
 @gin.configurable
-class AtariPreprocessing(object):
+class AtariPreprocessing(Wrapper):
   """A class implementing image preprocessing for Atari 2600 agents.
 
   Specifically, this provides the following subset from the JAIR paper
@@ -54,7 +55,7 @@ class AtariPreprocessing(object):
   """
 
   def __init__(self,
-               environment,
+               env,
                frame_skip=4,
                terminal_on_life_loss=False,
                screen_size=84):
@@ -70,6 +71,16 @@ class AtariPreprocessing(object):
     Raises:
       ValueError: if frame_skip or screen_size are not strictly positive.
     """
+    super(AtariPreprocessing, self).__init__(env)
+
+    # Return the observation space adjusted to match the shape of the processed
+    # observations.
+    self.observation_space = Box(
+        low=0,
+        high=255,
+        shape=(screen_size, screen_size, 1),
+        dtype=np.uint8)
+
     if frame_skip <= 0:
       raise ValueError(
           'Frame skip should be strictly positive, got {}'.format(frame_skip))
@@ -77,12 +88,11 @@ class AtariPreprocessing(object):
       raise ValueError('Target screen size should be strictly positive, got {}'
                        .format(screen_size))
 
-    self.environment = environment
     self.terminal_on_life_loss = terminal_on_life_loss
     self.frame_skip = frame_skip
     self.screen_size = screen_size
 
-    obs_dims = self.environment.observation_space
+    obs_dims = self.env.observation_space
     # Stores temporary observations used for pooling over two successive
     # frames.
     self.screen_buffer = [
@@ -93,28 +103,6 @@ class AtariPreprocessing(object):
     self.game_over = False
     self.lives = 0  # Will need to be set by reset().
 
-  @property
-  def observation_space(self):
-    # Return the observation space adjusted to match the shape of the processed
-    # observations.
-    return Box(
-        low=0,
-        high=255,
-        shape=(self.screen_size, self.screen_size, 1),
-        dtype=np.uint8)
-
-  @property
-  def action_space(self):
-    return self.environment.action_space
-
-  @property
-  def reward_range(self):
-    return self.environment.reward_range
-
-  @property
-  def metadata(self):
-    return self.environment.metadata
-
   def reset(self):
     """Resets the environment.
 
@@ -122,29 +110,12 @@ class AtariPreprocessing(object):
       observation: numpy array, the initial observation emitted by the
         environment.
     """
-    self.environment.reset()
-    self.lives = self.environment.ale.lives()
+    super(AtariPreprocessing, self).reset()
+    self.lives = self.env.ale.lives()
     self.game_over = False
     self._fetch_grayscale_observation(self.screen_buffer[0])
     self.screen_buffer[1].fill(0)
     return self._pool_and_resize()
-
-  def render(self, mode):
-    """Renders the current screen, before preprocessing.
-
-    This calls the Gym API's render() method.
-
-    Args:
-      mode: Mode argument for the environment's render() method.
-        Valid values (str) are:
-          'rgb_array': returns the raw ALE image.
-          'human': renders to display via the Gym renderer.
-
-    Returns:
-      if mode='rgb_array': numpy array, the most recent screen.
-      if mode='human': bool, whether the rendering was successful.
-    """
-    return self.environment.render(mode)
 
   def step(self, action):
     """Applies the given action in the environment.
@@ -172,11 +143,11 @@ class AtariPreprocessing(object):
     for time_step in range(self.frame_skip):
       # We bypass the Gym observation altogether and directly fetch the
       # grayscale image from the ALE. This is a little faster.
-      _, reward, game_over, info = self.environment.step(action)
+      _, reward, game_over, info = super(AtariPreprocessing, self).step(action)
       accumulated_reward += reward
 
       if self.terminal_on_life_loss:
-        new_lives = self.environment.ale.lives()
+        new_lives = self.env.ale.lives()
         is_terminal = game_over or new_lives < self.lives
         self.lives = new_lives
       else:
@@ -206,7 +177,7 @@ class AtariPreprocessing(object):
     Returns:
       observation: numpy array, the current observation in grayscale.
     """
-    self.environment.ale.getScreenGrayscale(output)
+    self.env.ale.getScreenGrayscale(output)
     return output
 
   def _pool_and_resize(self):
@@ -224,8 +195,8 @@ class AtariPreprocessing(object):
           self.screen_buffer[1],
           out=self.screen_buffer[0])
 
-    transformed_image = cv2.resize(
-        self.screen_buffer[0], (self.screen_size, self.screen_size),
-        interpolation=cv2.INTER_AREA)
-    int_image = np.asarray(transformed_image, dtype=np.uint8)
-    return np.expand_dims(int_image, axis=2)
+    transformed_image = tf.image.resize(
+        images=self.screen_buffer[0][..., np.newaxis],
+        size=(self.screen_size, self.screen_size),
+        method=tf.image.ResizeMethod.AREA)
+    return transformed_image.numpy().astype(np.uint8)
