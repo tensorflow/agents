ed172723345439f2636f2c8b5ed6634dfdb4cfb8
TF-Agents Team <no-reply@google.com>
2019-07-24 13:54:20 -0700

    extend ValueNetwork to handle complex observations with preprocessing_layer and preprocessing_combiner
    PiperOrigin-RevId: 259810453
    Change-Id: If72917d84513c3ae703ccf6d9efad8c9ff6c8baa

--- tf_agents/networks/value_network_test.py
+++ tf_agents/networks/value_network_test.py
@@ -31,7 +31,8 @@ class ValueNetworkTest(tf.test.TestCase):
 
   @test_util.run_in_graph_and_eager_modes()
   def testBuilds(self):
-    observation_spec = tensor_spec.BoundedTensorSpec((8, 8, 3), tf.int32, 0, 1)
+    observation_spec = tensor_spec.BoundedTensorSpec((8, 8, 3), tf.float32, 0,
+                                                     1)
     observation = tensor_spec.sample_spec_nest(
         observation_spec, outer_dims=(1,))
 
@@ -59,7 +60,8 @@ class ValueNetworkTest(tf.test.TestCase):
 
   @test_util.run_in_graph_and_eager_modes()
   def testHandlesExtraOuterDims(self):
-    observation_spec = tensor_spec.BoundedTensorSpec((8, 8, 3), tf.int32, 0, 1)
+    observation_spec = tensor_spec.BoundedTensorSpec((8, 8, 3), tf.float32, 0,
+                                                     1)
     observation = tensor_spec.sample_spec_nest(
         observation_spec, outer_dims=(3, 3, 2))
 
@@ -68,6 +70,28 @@ class ValueNetworkTest(tf.test.TestCase):
 
     value, _ = net(observation)
     self.assertEqual([3, 3, 2], value.shape.as_list())
+
+  @test_util.run_in_graph_and_eager_modes()
+  def testHandlePreprocessingLayers(self):
+    observation_spec = (tensor_spec.TensorSpec([1], tf.float32),
+                        tensor_spec.TensorSpec([], tf.float32))
+    observation = tensor_spec.sample_spec_nest(
+        observation_spec, outer_dims=(3,))
+
+    preprocessing_layers = (tf.keras.layers.Dense(4),
+                            tf.keras.Sequential([
+                                tf.keras.layers.Reshape((1,)),
+                                tf.keras.layers.Dense(4)
+                            ]))
+
+    net = value_network.ValueNetwork(
+        observation_spec,
+        preprocessing_layers=preprocessing_layers,
+        preprocessing_combiner=tf.keras.layers.Add())
+
+    value, _ = net(observation)
+    self.assertEqual([3], value.shape.as_list())
+    self.assertGreater(len(net.trainable_variables), 4)
 
 
 if __name__ == '__main__':
