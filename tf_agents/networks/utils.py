ab4f02d15ab29975199518af68f211d2a0627181
Anoop Korattikara <kbanoop@google.com>
2019-11-12 13:38:31 -0800

    Add weight decay to utils.mlp_layers
    PiperOrigin-RevId: 280043456
    Change-Id: I0b268f00a0fed2a35aa67a428280ca5aa2d41c70

--- tf_agents/networks/utils.py
+++ tf_agents/networks/utils.py
@@ -108,6 +108,7 @@ def mlp_layers(conv_layer_params=None,
                dropout_layer_params=None,
                activation_fn=tf.keras.activations.relu,
                kernel_initializer=None,
+               weight_decay_params=None,
                name=None):
   """Generates conv and fc layers to encode into a hidden state.
 
@@ -129,6 +130,8 @@ def mlp_layers(conv_layer_params=None,
     kernel_initializer: Initializer to use for the kernels of the conv and
       dense layers. If none is provided a default variance_scaling_initializer
       is used.
+    weight_decay_params: Optional list of weight decay params for the fully
+      connected layer.
     name: Name for the mlp layers.
 
   Returns:
@@ -165,11 +168,25 @@ def mlp_layers(conv_layer_params=None,
         raise ValueError('Dropout and full connected layer parameter lists have'
                          ' different lengths (%d vs. %d.)' %
                          (len(dropout_layer_params), len(fc_layer_params)))
-    for num_units, dropout_params in zip(fc_layer_params, dropout_layer_params):
+
+    if weight_decay_params is None:
+      weight_decay_params = [None] * len(fc_layer_params)
+    else:
+      if len(weight_decay_params) != len(fc_layer_params):
+        raise ValueError('Weight decay and fully connected layer parameter '
+                         'lists have different lengths (%d vs. %d.)' %
+                         (len(weight_decay_params), len(fc_layer_params)))
+
+    for num_units, dropout_params, weight_decay in zip(
+        fc_layer_params, dropout_layer_params, weight_decay_params):
+      kernel_regularizer = None
+      if weight_decay is not None:
+        kernel_regularizer = tf.keras.regularizers.l2(weight_decay)
       layers.append(tf.keras.layers.Dense(
           num_units,
           activation=activation_fn,
           kernel_initializer=kernel_initializer,
+          kernel_regularizer=kernel_regularizer,
           name='/'.join([name, 'dense']) if name else None))
       if not isinstance(dropout_params, dict):
         dropout_params = {'rate': dropout_params} if dropout_params else None
