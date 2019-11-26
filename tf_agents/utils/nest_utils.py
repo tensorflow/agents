b9022108e4252773995ac34429159f0963654339
Michael H. Evangelista <michael@powerof.to>
2019-11-26 12:36:58 -0500

    Changed lambda to separate function for clarity per suggestion.

--- tf_agents/utils/nest_utils.py
+++ tf_agents/utils/nest_utils.py
@@ -560,9 +560,10 @@ def where(condition, true_outputs, false_outputs):
     Interleaved output from `true_outputs` and `false_outputs` based on
     `condition`.
   """
-  return tf.nest.map_structure(
-    lambda t, f: tf.where(
-      tf.reshape(condition,
-                 tf.concat([tf.shape(condition), tf.ones(tf.rank(t) - tf.rank(condition), dtype=tf.int32)], axis=0)),
-      t, f),
-    true_outputs, false_outputs)
+  def where_with_matching_ranks(t, f):
+      rank_difference = tf.rank(t) - tf.rank(condition)
+      condition_shape = tf.concat([tf.shape(condition), tf.ones(rank_difference, dtype=tf.int32)], axis=0)
+      reshaped_condition = tf.reshape(condition, condition_shape)
+      return tf.where(reshaped_condition, t, f)
+
+  return tf.nest.map_structure(where_with_matching_ranks, true_outputs, false_outputs)
