91ad5bed12232027be47bd38b6691324f765f919
Toby Boyd <tobyboyd@google.com>
2020-04-03 14:51:37 -0700

    Change to stable release 0.4.0.

--- tf_agents/version.py
+++ tf_agents/version.py
@@ -26,7 +26,7 @@ _PATCH_VERSION = '0'
 # release branch, the current version is by default assumed to be a
 # 'development' version, labeled 'dev'.
 _DEV_SUFFIX = 'dev'
-_REL_SUFFIX = 'rc0'
+_REL_SUFFIX = ''
 
 # Example, '0.4.0rc0'
 __version__ = '.'.join([
