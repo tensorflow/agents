Fix the RandomTFPolicy's calculation of log_probability. 1) When the action is non-scalar, we emit the marginal lob prob of the joint distribution. 2) We now respect the batch dimensions.
PiperOrigin-RevId: 314569196
Change-Id: Id79f983679263f9ce28842731b649e5f80ec16de

tf_agents/policies/random_tf_policy.py
@@ -18,7 +18,6 @@ from __future__ import absolute_import
 from __future__ import division
 # Using Type Annotations.
 from __future__ import print_function
-
 import tensorflow as tf  # pylint: disable=g-explicit-tensorflow-version-import
 from tf_agents.distributions import masked
 from tf_agents.policies import tf_policy
@@ -29,13 +28,34 @@ from tf_agents.typing import types
 from tf_agents.utils import nest_utils
 
 
-def _uniform_probability(action_spec):
-  """Helper function for returning probabilities of equivalent distributions."""
+def _calculate_log_probability(outer_dims, action_spec):
+  """Helper function for calculating log prob of a uniform distribution.
+
+  Each item in the returned tensor will be equal to:
+  |action_spec.shape| * log_prob_of_each_component_of_action_spec.
+
+  Note that this method expects the same value for all outer_dims because
+  we're sampling uniformly from the same distribution for each batch row.
+
+  Args:
+    outer_dims: TensorShape.
+    action_spec: BoundedTensorSpec.
+
+  Returns:
+    A tensor of type float32 with shape outer_dims.
+  """
   # Equivalent of what a tfp.distribution.Categorical would return.
   if action_spec.dtype.is_integer:
-    return 1. / (action_spec.maximum - action_spec.minimum + 1)
+    log_prob = -tf.math.log(action_spec.maximum - action_spec.minimum + 1.0)
   # Equivalent of what a tfp.distribution.Uniform would return.
-  return 1. / (action_spec.maximum - action_spec.minimum)
+  else:
+    log_prob = -tf.math.log(action_spec.maximum - action_spec.minimum)
+
+  # Note that log_prob may be a vector. We first reduce it to a scalar, and then
+  # adjust by the number of times that vector is repeated in action_spec.
+  log_prob = tf.reduce_sum(log_prob) * (
+      action_spec.shape.num_elements() / log_prob.shape.num_elements())
+  return tf.fill(outer_dims, log_prob)
 
 
 class RandomTFPolicy(tf_policy.TFPolicy):
@@ -43,6 +63,9 @@ class RandomTFPolicy(tf_policy.TFPolicy):
 
   Note: the values in the info_spec (except for the log_probability) are random
     values that have nothing to do with the emitted actions.
+
+  Note: The returned info.log_probabiliy will be an object matching the
+  structure of action_spec, where each value is a tensor of size [batch_size].
   """
 
   def __init__(self, time_step_spec: ts.TimeStep,
@@ -119,9 +142,9 @@ class RandomTFPolicy(tf_policy.TFPolicy):
         log_probability = masked_categorical.log_prob(action_ -
                                                       self.action_spec.minimum)
       else:
-        action_probability = tf.nest.map_structure(_uniform_probability,
-                                                   self._action_spec)
-        log_probability = tf.nest.map_structure(tf.math.log, action_probability)
+        log_probability = tf.nest.map_structure(
+            lambda s: _calculate_log_probability(outer_dims, s),
+            self._action_spec)
       policy_info = policy_step.set_log_probability(policy_info,
                                                     log_probability)
 

tf_agents/policies/random_tf_policy_test.py
@@ -19,9 +19,11 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import math
+
 from absl.testing import parameterized
 import numpy as np
-import tensorflow as tf  # pylint: disable=g-explicit-tensorflow-version-import
+import tensorflow as tf
 from tf_agents.policies import random_tf_policy
 from tf_agents.specs import tensor_spec
 from tf_agents.trajectories import time_step as ts
@@ -117,6 +119,9 @@ class RandomTFPolicyTest(test_utils.TestCase, parameterized.TestCase):
         tensor_spec.BoundedTensorSpec((1, 2), dtype, -10, 10)
     ]
     time_step_spec, time_step = self.create_time_step()
+    batch_size = 3
+    time_step = self.create_batch(time_step, batch_size)
+
     policy = random_tf_policy.RandomTFPolicy(
         time_step_spec=time_step_spec,
         action_spec=action_spec,
@@ -131,7 +136,10 @@ class RandomTFPolicyTest(test_utils.TestCase, parameterized.TestCase):
     p = 1. / 21 if dtype.is_integer else 1. / 20
     np.testing.assert_allclose(
         np.array(step.info.log_probability, dtype=np.float32),
-        np.array(np.log([p, p], dtype=np.float32)),
+        np.array(
+            np.log([[math.pow(p, 6) for _ in range(3)],
+                    [math.pow(p, 2) for _ in range(3)]]),
+            dtype=np.float32),
         rtol=1e-5)
     self.assertTrue(np.all(action_[0] >= -10))
     self.assertTrue(np.all(action_[0] <= 10))