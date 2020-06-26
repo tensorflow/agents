4e12573500392356220777692de51f551dec8841
TF-Agents Team <no-reply@google.com>
2020-06-26 06:03:58 -0700

    Move multi-objective scalarizer code to a separate directory under tf_agents/bandits
    PiperOrigin-RevId: 318457718
    Change-Id: Iaf6999376d7cbcda5999a5cfd344d472e0207c9a

--- tf_agents/bandits/multi_objective/multi_objective_scalarizer_test.py
+++ tf_agents/bandits/multi_objective/multi_objective_scalarizer_test.py
@@ -22,7 +22,7 @@ from __future__ import print_function
 import copy
 import tensorflow.compat.v2 as tf
 
-from tf_agents.bandits.agents import multi_objective_scalarizer
+from tf_agents.bandits.multi_objective import multi_objective_scalarizer
 
 
 class DummyScalarizer(multi_objective_scalarizer.Scalarizer):
