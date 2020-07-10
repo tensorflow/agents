6fe67ea03e3644c3a3569f904f068dc036cce662
孔令超 <kong.lingchao@gmail.com>
2020-07-10 13:48:53 +0800

    import encoding_network

--- tf_agents/agents/ddpg/critic_network.py
+++ tf_agents/agents/ddpg/critic_network.py
@@ -19,6 +19,7 @@ import gin
 import tensorflow as tf  # pylint: disable=g-explicit-tensorflow-version-import
 
 from tf_agents.networks import network
+from tf_agents.networks import encoding_network
 from tf_agents.networks import utils
 
 
