Fix bug in random_tf_policy where it was not masking output space when specifying per_arm_features with NUM_ACTIONS.
PiperOrigin-RevId: 320705369
Change-Id: Ib4f6c39f6fc85f170872d60afaa663487eaa21bf

tf_agents/policies/random_tf_policy.py
@@ -19,6 +19,8 @@ from __future__ import division
 # Using Type Annotations.
 from __future__ import print_function
 import tensorflow as tf  # pylint: disable=g-explicit-tensorflow-version-import
+from tf_agents.bandits.policies import policy_utilities
+from tf_agents.bandits.specs import utils as bandit_spec_utils
 from tf_agents.distributions import masked
 from tf_agents.policies import tf_policy
 from tf_agents.specs import tensor_spec
@@ -59,6 +61,7 @@ def _calculate_log_probability(outer_dims, action_spec):
   return tf.cast(tf.fill(outer_dims, log_prob), tf.float32)
 
 
+# TODO(b/161005095): Refactor into RandomTFPolicy and RandomBanditTFPolicy.
 class RandomTFPolicy(tf_policy.TFPolicy):
   """Returns random samples of the given action_spec.
 
@@ -120,18 +123,35 @@ class RandomTFPolicy(tf_policy.TFPolicy):
     else:
       observation = time_step.observation
 
-      action_ = tensor_spec.sample_spec_nest(
-          self._action_spec, seed=seed, outer_dims=outer_dims)
+      if self._accepts_per_arm_features:
+        max_num_arms = self._action_spec.maximum - self._action_spec.minimum + 1
+        batch_size = tf.shape(time_step.step_type)[0]
+        num_actions = observation.get(
+            bandit_spec_utils.NUM_ACTIONS_FEATURE_KEY,
+            tf.ones(shape=(batch_size,), dtype=tf.int32) * max_num_arms)
+        mask = tf.sequence_mask(num_actions, max_num_arms)
+        zero_logits = tf.cast(tf.zeros_like(mask), tf.float32)
+        masked_categorical = masked.MaskedCategorical(zero_logits, mask)
+        action_ = tf.nest.map_structure(
+            lambda t: tf.cast(masked_categorical.sample() + t.minimum, t.dtype),
+            self._action_spec)
+      else:
+        action_ = tensor_spec.sample_spec_nest(
+            self._action_spec, seed=seed, outer_dims=outer_dims)
+
       policy_info = tensor_spec.sample_spec_nest(
           self._info_spec, outer_dims=outer_dims)
+
+    # Update policy info with chosen arm features.
     if self._accepts_per_arm_features:
       def _gather_fn(t):
         return tf.gather(params=t, indices=action_, batch_dims=1)
+      chosen_arm_features = tf.nest.map_structure(
+          _gather_fn, observation[bandit_spec_utils.PER_ARM_FEATURE_KEY])
 
-      chosen_arm_features = tf.nest.map_structure(_gather_fn,
-                                                  observation['per_arm'])
-      policy_info = policy_info._replace(
-          chosen_arm_features=chosen_arm_features)
+      if policy_utilities.has_chosen_arm_features(self._info_spec):
+        policy_info = policy_info._replace(
+            chosen_arm_features=chosen_arm_features)
 
     # TODO(b/78181147): Investigate why this control dependency is required.
     if time_step is not None:
@@ -139,7 +159,8 @@ class RandomTFPolicy(tf_policy.TFPolicy):
         action_ = tf.nest.map_structure(tf.identity, action_)
 
     if self.emit_log_probability:
-      if observation_and_action_constraint_splitter is not None:
+      if (self._accepts_per_arm_features
+          or observation_and_action_constraint_splitter is not None):
         log_probability = masked_categorical.log_prob(action_ -
                                                       self.action_spec.minimum)
       else:

tf_agents/policies/random_tf_policy_test.py
@@ -24,6 +24,8 @@ import math
 from absl.testing import parameterized
 import numpy as np
 import tensorflow as tf
+from tf_agents.bandits.policies import policy_utilities
+from tf_agents.bandits.specs import utils as bandit_spec_utils
 from tf_agents.policies import random_tf_policy
 from tf_agents.specs import tensor_spec
 from tf_agents.trajectories import time_step as ts
@@ -44,12 +46,29 @@ class RandomTFPolicyTest(test_utils.TestCase, parameterized.TestCase):
                                                       batch_size)
     return batch_time_step
 
-  def create_time_step(self):
+  def create_time_step(self, use_per_arm_features=False, num_arms=1):
     observation = tf.constant([[1, 2], [3, 4]], dtype=tf.float32)
-    time_step = ts.restart(observation)
-
     observation_spec = tensor_spec.TensorSpec(observation.shape.as_list(),
                                               tf.float32)
+    if use_per_arm_features:
+      # Create arm features with:
+      # max_num_arms = 4, num_action = 2, per_arm_dim = 2.
+      observation = {
+          bandit_spec_utils.GLOBAL_FEATURE_KEY: observation,
+          bandit_spec_utils.PER_ARM_FEATURE_KEY:
+              tf.constant([[5, 6],
+                           [7, 8],
+                           [9, 10],
+                           [11, 12]],
+                          tf.float32),
+          bandit_spec_utils.NUM_ACTIONS_FEATURE_KEY:
+              tf.constant(num_arms, tf.int32),
+      }
+      observation_spec = tf.nest.map_structure(
+          lambda t: tensor_spec.TensorSpec(t.shape.as_list(), t.dtype),
+          observation)
+
+    time_step = ts.restart(observation)
     time_step_spec = ts.time_step_spec(observation_spec)
 
     return time_step_spec, time_step
@@ -190,6 +209,55 @@ class RandomTFPolicyTest(test_utils.TestCase, parameterized.TestCase):
     # With only three valid actions, all of the probabilities should be 1/3.
     self.assertAllClose(step.info.log_probability,
                         tf.constant(np.log(1. / 3), shape=[batch_size]))
+
+  def testNumActions(self, dtype):
+    if not dtype.is_integer:
+      self.skipTest('testNumActions only applies to integer dtypes')
+
+    batch_size = 1000
+
+    # Create action spec, time_step and spec with max_num_arms = 4.
+    action_spec = tensor_spec.BoundedTensorSpec((), dtype, 0, 3)
+    time_step_spec, time_step_1 = self.create_time_step(
+        use_per_arm_features=True, num_arms=2)
+    _, time_step_2 = self.create_time_step(
+        use_per_arm_features=True, num_arms=3)
+    # First half of time_step batch will have num_action = 2 and second
+    # half will have num_actions = 3.
+    half_batch_size = int(batch_size / 2)
+    time_step = nest_utils.stack_nested_tensors(
+        [time_step_1] * half_batch_size + [time_step_2] * half_batch_size)
+
+    # The features for the chosen arm is saved to policy_info.
+    chosen_arm_features_info = (
+        policy_utilities.create_chosen_arm_features_info_spec(
+            time_step_spec.observation))
+    info_spec = policy_utilities.PerArmPolicyInfo(
+        chosen_arm_features=chosen_arm_features_info)
+
+    policy = random_tf_policy.RandomTFPolicy(
+        time_step_spec=time_step_spec,
+        action_spec=action_spec,
+        info_spec=info_spec,
+        accepts_per_arm_features=True,
+        emit_log_probability=True)
+
+    action_step = policy.action(time_step)
+    tf.nest.assert_same_structure(action_spec, action_step.action)
+
+    # Sample from the policy 1000 times, and ensure that actions considered
+    # invalid according to the mask are never chosen.
+    step = self.evaluate(action_step)
+    action_ = step.action
+    self.assertTrue(np.all(action_ >= 0))
+    self.assertTrue(np.all(action_[:half_batch_size] < 2))
+    self.assertTrue(np.all(action_[half_batch_size:] < 3))
+
+    # With num_action valid actions, probabilities should be 1/num_actions.
+    self.assertAllClose(step.info.log_probability[:half_batch_size],
+                        tf.constant(np.log(1. / 2), shape=[half_batch_size]))
+    self.assertAllClose(step.info.log_probability[half_batch_size:],
+                        tf.constant(np.log(1. / 3), shape=[half_batch_size]))
 
   def testInfoSpec(self, dtype):
     action_spec = [