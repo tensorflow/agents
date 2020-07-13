903ddd783d01dcb11af21971b0f8c2a4690fd7ae
Xiaojian Ma <jxma@google.com>
2020-07-13 16:02:56 -0700

    Add test for `NestedCountingEnv`
    PiperOrigin-RevId: 321052509
    Change-Id: I8b107b6b1098295d470b7c1f391cab58f13cbf9f

--- tf_agents/environments/test_envs_test.py
+++ tf_agents/environments/test_envs_test.py
@@ -68,5 +68,40 @@ class EpisodeCountingEnvTest(test_utils.TestCase):
     env = test_envs.EpisodeCountingEnv(steps_per_episode=15)
     env_utils.validate_py_environment(env, episodes=10)
 
+
+class NestedCountingEnvTest(test_utils.TestCase):
+
+  def test_sequential(self):
+    num_episodes = 3
+    steps_per_episode = 4
+    env = test_envs.NestedCountingEnv(steps_per_episode)
+
+    for episode in range(num_episodes):
+      step = 0
+      time_step = env.reset()
+      self.assertCountEqual(
+          {
+              'total_steps': episode * 10 + step,
+              'current_steps': step,
+          }, time_step.observation)
+      while not time_step.is_last():
+        time_step = env.step(0)
+        step += 1
+        self.assertCountEqual(
+            {
+                'total_steps': episode * 10 + step,
+                'current_steps': step,
+            }, time_step.observation)
+      self.assertCountEqual(
+          {
+              'total_steps': episode * 10 + steps_per_episode,
+              'current_steps': steps_per_episode,
+          }, time_step.observation)
+
+  def test_validate_specs(self):
+    env = test_envs.NestedCountingEnv(steps_per_episode=15)
+    env_utils.validate_py_environment(env, episodes=10)
+
+
 if __name__ == '__main__':
   test_utils.main()
