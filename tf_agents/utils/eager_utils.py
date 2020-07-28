[tf_agents] Fix gradients in eager_utils.
In Python 3, zip returns an iterator. If summarize_gradients is true, grads_and_vars iterator will be exhausted by add_gradients_summaries and then becomes empty when optimizer.apply_gradients is called.

PiperOrigin-RevId: 323666966
Change-Id: I83e57975a1cd7fa091fbfb06bab76cf825455272

tf_agents/utils/eager_utils.py
@@ -346,7 +346,7 @@ def create_train_step(loss,
     variables_to_train = variables_to_train()
   variables_to_train = tf.nest.flatten(variables_to_train)
   grads = tape.gradient(total_loss_value, variables_to_train)
-  grads_and_vars = zip(grads, variables_to_train)
+  grads_and_vars = list(zip(grads, variables_to_train))
 
   if transform_grads_fn:
     grads_and_vars = transform_grads_fn(grads_and_vars)