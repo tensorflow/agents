f217b1ffafc9852a1c08ad1781ee2f0a89d347b8
Emily Fertig <emilyaf@google.com>
2020-08-24 15:37:50 -0700

    Pass bijector parameters into base class constructor for compatibility with bijector caching changes.
    PiperOrigin-RevId: 328222871
    Change-Id: Ifd8b72981b5cd4137762bbee581f995fd25aa929

--- tf_agents/distributions/tanh_bijector_stable.py
+++ tf_agents/distributions/tanh_bijector_stable.py
@@ -44,9 +44,11 @@ class Tanh(bijector.Bijector):
   """
 
   def __init__(self, validate_args=False, name="tanh"):
+    parameters = dict(locals())
     super(Tanh, self).__init__(
         forward_min_event_ndims=0,
         validate_args=validate_args,
+        parameters=parameters,
         name=name)
 
   def _forward(self, x):
