1dd3a4f8c9df0d06aff944584c2a8da02dd48ab5
Eugene Brevdo <ebrevdo@google.com>
2020-09-02 13:43:30 -0700

    [TF-Agents] New utilities to manipulate/serialize tf.Distributions' parameters.
    This PR adds two new converters to tf_agents.distributions.utils.
    
    PiperOrigin-RevId: 329779489
    Change-Id: I5e4596017e4fac533e1c5294db05dc641be980c2

--- tf_agents/distributions/utils.py
+++ tf_agents/distributions/utils.py
@@ -17,12 +17,18 @@
 
 from __future__ import absolute_import
 from __future__ import division
+# Using Type Annotations.
 from __future__ import print_function
 
-import tensorflow as tf  # pylint: disable=g-explicit-tensorflow-version-import
+import inspect
+from typing import Any, Mapping, Type, Text
+
+import tensorflow as tf
 import tensorflow_probability as tfp
+
 from tf_agents.distributions import tanh_bijector_stable
 from tf_agents.utils import common
+from tf_agents.utils import nest_utils
 
 
 def scale_distribution_to_spec(distribution, spec):
@@ -157,3 +163,329 @@ class SquashToSpecNormal(tfp.distributions.Distribution):
   def batch_shape_tensor(self, name="batch_shape_tensor"):
     """Compute event shape tensor of the SquashToSpecNormal distribution."""
     return self._squashed_distribution.batch_shape_tensor(name)
+
+
+class Params(object):
+  """The (recursive) parameters of objects exposing the `parameters` property.
+
+  This includes TFP `Distribution`, `Bijector`, and TF `LinearOperator`.
+
+  `Params` objects are created with
+  `tf_agents.distributions.utils.get_parameters`;
+  `Params` can be converted back to original objects via
+  `tf_agents.distributions.utils.make_from_parameters`.
+
+  In-place edits of fields are allowed, and will not modify the original
+  objects (with the exception of, e.g., reference objects like `tf.Variable`
+  being modified in-place).
+
+  The components of a `Params` object are: `type_` and `params`.
+
+  - `type_` is the type of object.
+  - `params` is a `dict` of the (non-default) non-tensor arguments passed to the
+    object's `__init__`; and includes nests of Python objects, as well as other
+    `Params` values representing "Param-representable" objects passed to init.
+
+  A non-trivial example:
+
+  ```python
+  scale_matrix = tf.Variable([[1.0, 2.0], [-1.0, 0.0]])
+  d = tfp.distributions.MultivariateNormalDiag(
+      loc=[1.0, 1.0], scale_diag=[2.0, 3.0], validate_args=True)
+  b = tfp.bijectors.ScaleMatvecLinearOperator(
+      scale=tf.linalg.LinearOperatorFullMatrix(matrix=scale_matrix),
+      adjoint=True)
+  b_d = b(d)
+  p = utils.get_parameters(b_d)
+  ```
+
+  Then `p` is:
+
+  ```python
+  Params(
+      tfp.distributions.TransformedDistribution,
+      params={
+          "bijector": Params(
+              tfp.bijectors.ScaleMatvecLinearOperator,
+              params={"adjoint": True,
+                      "scale": Params(
+                          tf.linalg.LinearOperatorFullMatrix,
+                          params={"matrix": scale_matrix})}),
+          "distribution": Params(
+              tfp.distributions.MultivariateNormalDiag,
+              params={"validate_args": True,
+                      "scale_diag": [2.0, 3.0],
+                      "loc": [1.0, 1.0]})})
+  ```
+
+  This structure can be manipulated and/or converted back to a `Distribution`
+  instance via `make_from_parameters`:
+
+  ```python
+  p.params["distribution"].params["loc"] = [0.0, 0.0]
+
+  # The distribution `new_b_d` will be a MVN centered on `(0, 0)` passed through
+  # the `ScaleMatvecLinearOperator` bijector.
+  new_b_d = utils.make_from_parameters(p)
+  ```
+  """
+  type_: Type[Any]  # Any class that has a .parameters.
+  params: Mapping[Text, Any]
+
+  def __str__(self):
+    return "<Params: type={}, params={}".format(self.type_, self.params)
+
+  def __repr__(self):
+    return str(self)
+
+  def __init__(self, type_, params):
+    self.type_ = type_
+    self.params = params
+
+
+def get_parameters(value: Any) -> Params:
+  """Creates a recursive `Params` object from `value`.
+
+  The `Params` object can be converted back to an object of type `type(value)`
+  with `make_from_parameters`.  For more details, see the docstring of
+  `Params`.
+
+  Args:
+    value: Typically a user provides `tfp.Distribution`, `tfp.Bijector`, or
+      `tf.linalg.LinearOperator`, but this can be any Python object.
+
+  Returns:
+    An instance of `Params`.
+
+  Raises:
+    TypeError: If `value.parameters` exists, is not `None`, but but is also not
+       a `Mapping` (e.g. a `dict`).
+  """
+  parameters = getattr(value, "parameters", None)
+  if not isinstance(parameters, Mapping):
+    raise TypeError(
+        "value.parameters is not available or is not a dict; "
+        "value: {}; parameters: {}".format(value, parameters))
+  type_ = type(value)
+  params = {}
+
+  def process_parameter(p):
+    if getattr(p, "parameters", None) is not None:
+      return get_parameters(p)
+    else:
+      return p
+
+  if getattr(value, "parameters"):
+    default_values = inspect.signature(type_).parameters.items()
+    default_values = {
+        k: v.default
+        for (k, v) in default_values
+        if v.default is not inspect.Parameter.empty
+    }
+    params = {
+        k: tf.nest.map_structure(process_parameter, v)
+        for k, v in value.parameters.items()
+        if v is not default_values.get(k, None)
+    }
+    return Params(type(value), params)
+
+  return value
+
+
+def make_from_parameters(value: Params) -> Any:
+  """Creates an instance of type `value.type_` with the parameters in `value`.
+
+  For more details, see the docstrings for `get_parameters` and `Params`.
+
+  This function may raise strange errors if `value` is a `Params` created from
+  a badly constructed object (one which does not set `self._parameters`
+  properly).  For example:
+
+  ```python
+  class MyBadlyConstructedDistribution(tfp.distributions.Categorical):
+    def __init__(self, extra_arg, **kwargs):
+      super().__init__(**kwargs)
+      self._extra_arg = extra_arg
+
+    ...
+  ```
+
+  To fix this, make sure `self._parameters` are properly set:
+
+  ```python
+  class MyProperlyConstructedDistribution(tfp.distributions.Categorical):
+    def __init__(self, extra_arg, **kwargs):
+      super().__init__(**kwargs)
+      # Ensure all arguments to `__init__` are in `self._parameters`.
+      self._parameters = dict(extra_arg=extra_arg, **kwargs)
+      self._extra_arg = extra_arg
+
+    ...
+  ```
+
+  Args:
+    value: A `Params` object; the output of `get_parameters` (or a
+      modified version thereof).
+
+  Returns:
+    An instance of `value.type_`.
+
+  Raises:
+    Exception: If `value` is a `Params` object and the initializer of
+      `value.type_` does not recognize accept the args structure given in
+      `value.params`.  This can happen if, e.g., `value.type_.__init__` does not
+      properly set `self._parameters` or `self.parameters` to match the
+      arguments it expects.
+  """
+  def make_from_params_or_identity(v_):
+    return make_from_parameters(v_) if isinstance(v_, Params) else v_
+
+  params = {
+      k: tf.nest.map_structure(make_from_params_or_identity, v)
+      for k, v in value.params.items()
+  }
+  return value.type_(**params)
+
+
+def parameters_to_dict(value: Params) -> Mapping[Text, Any]:
+  """Converts `value` to a nested `dict` (excluding all `type_` info).
+
+  Sub-dicts represent `Params` objects; keys represent flattened nest structures
+  in `value.params`.
+
+  Example:
+
+  ```python
+  scale_matrix = tf.Variable([[1.0, 2.0], [-1.0, 0.0]])
+  d = tfp.distributions.MultivariateNormalDiag(
+      loc=[1.0, 1.0], scale_diag=[2.0, 3.0], validate_args=True)
+  b = tfp.bijectors.ScaleMatvecLinearOperator(
+      scale=tf.linalg.LinearOperatorFullMatrix(matrix=scale_matrix),
+      adjoint=True)
+  b_d = b(d)
+  p = utils.get_parameters(b_d)
+  params_dict = utils.parameters_to_dict(p)
+  ```
+
+  results in the nested dictionary:
+
+  ```python
+  {"bijector": {"adjoint": True,
+                "scale": {"matrix": scale_matrix}},
+   "distribution": {"validate_args": True,
+                    # These are deeply nested because we passed lists
+                    # intead of numpy arrays for `loc` and `scale_diag`.
+                    "scale_diag:0": 2.0,
+                    "scale_diag:1": 3.0,
+                    "loc:0": 1.0,
+                    "loc:1": 1.0}
+  }
+  ```
+
+  The dictionary may then be modified or updated (e.g., in place), and converted
+  back to a `Params` object using `merge_to_parameters_from_dict`.
+
+  Args:
+    value: The (possibly recursively defined) `Params`.
+
+  Returns:
+    A `dict` mapping `value.params` to flattened key/value pairs.  Any
+    sub-`Params` objects become nested dicts.
+  """
+  convert = lambda p: parameters_to_dict(p) if isinstance(p, Params) else p
+
+  output_entries = {}
+  for k, v in value.params.items():
+    if tf.nest.is_nested(v):
+      flattened_params = nest_utils.flatten_with_joined_paths(v)
+      for (param_k, param_v) in flattened_params:
+        key = "{}:{}".format(k, param_k)
+        output_entries[key] = convert(param_v)
+    else:
+      output_entries[k] = convert(v)
+  return output_entries
+
+
+def merge_to_parameters_from_dict(
+    value: Params, params_dict: Mapping[Text, Any]) -> Params:
+  """Merges dict matching data of `parameters_to_dict(value)` to a new `Params`.
+
+  For more details, see the example below and the documentation of
+  `parameters_to_dict`.
+
+  Example:
+
+  ```python
+  scale_matrix = tf.Variable([[1.0, 2.0], [-1.0, 0.0]])
+  d = tfp.distributions.MultivariateNormalDiag(
+      loc=[1.0, 1.0], scale_diag=[2.0, 3.0], validate_args=True)
+  b = tfp.bijectors.ScaleMatvecLinearOperator(
+      scale=tf.linalg.LinearOperatorFullMatrix(matrix=scale_matrix),
+      adjoint=True)
+  b_d = b(d)
+  p = utils.get_parameters(b_d)
+
+  params_dict = utils.parameters_to_dict(p)
+  params_dict["bijector"]["scale"]["matrix"] = new_scale_matrix
+
+  new_params = utils.merge_to_parameters_from_dict(
+    p, params_dict)
+
+  # new_d is a `ScaleMatvecLinearOperator()(MultivariateNormalDiag)` with
+  # a new scale matrix.
+  new_d = utils.make_from_parameters(new_params)
+  ```
+
+  Args:
+    value: A `Params` from which `params_dict` was derived.
+    params_dict: A nested `dict` created by e.g. calling
+      `parameters_to_dict(value)` and  modifying it to modify parameters.
+      **NOTE** If any keys in the dict are missing, the "default" value in
+      `value` is used instead.
+
+  Returns:
+    A new `Params` object which can then be turned into e.g. a
+    `tfp.Distribution` via `make_from_parameters`.
+
+  Raises:
+    ValueError: If `params_dict` has keys missing from `value.params`.
+    KeyError: If a subdict entry is missing for a nested value in
+      `value.params`.
+  """
+
+  new_params = {}
+
+  processed_params = set()
+  for k, v in value.params.items():
+    # pylint: disable=cell-var-from-loop
+    def convert(params_k, p):
+      if params_k is not None:
+        params_key = "{}:{}".format(k, params_k)
+        params_dict_value = params_dict.get(params_key, None)
+        if params_dict_value is None:
+          raise KeyError("Missing a required nested element from "
+                         "params_dict.keys: '{}'.  params_dict.keys: {}"
+                         .format(params_key, sorted(params_dict.keys())))
+      else:
+        params_key = k
+        params_dict_value = params_dict.get(k, None)
+      processed_params.add(params_key)
+      if isinstance(p, Params):
+        return merge_to_parameters_from_dict(p, params_dict_value)
+      else:
+        return params_dict_value if params_dict_value is not None else p
+    # pylint: enable=cell-var-from-loop
+
+    if tf.nest.is_nested(v):
+      new_params[k] = nest_utils.map_structure_with_paths(convert, v)
+    else:
+      new_params[k] = convert(None, v)
+
+  unvisited_params_keys = set(params_dict) - processed_params
+  if unvisited_params_keys:
+    raise ValueError(
+        "params_dict had keys that were not part of value.params.  "
+        "params_dict keys: {}, value.params processed keys: {}".format(
+            sorted(params_dict.keys()), sorted(processed_params)))
+
+  return Params(type_=value.type_, params=new_params)
--- tf_agents/distributions/utils_test.py
+++ tf_agents/distributions/utils_test.py
@@ -17,10 +17,13 @@
 
 from __future__ import absolute_import
 from __future__ import division
+# Using Type Annotations.
 from __future__ import print_function
 
-import tensorflow as tf  # pylint: disable=g-explicit-tensorflow-version-import
+import numpy as np
+import tensorflow as tf
 import tensorflow_probability as tfp
+
 from tf_agents.distributions import utils
 from tf_agents.specs import tensor_spec
 
@@ -56,6 +59,183 @@ class UtilsTest(tf.test.TestCase):
     squash_to_spec_normal = utils.SquashToSpecNormal(input_dist, action_spec)
     self.assertAlmostEqual(
         self.evaluate(squash_to_spec_normal.stddev()), 2.98516426, places=5)
+
+  def compare_params(self, p1, p2, skip_tensor_values=False):
+    if tf.is_tensor(p1):
+      # When comparing tensors, just make sure both items are tensor-like.  This
+      # allows us to consider, e.g., tf.Variable and tf.Tensor equivalent.
+      self.assertTrue(tf.is_tensor(p2))
+    else:
+      self.assertFalse(tf.is_tensor(p2))
+      self.assertEqual(type(p1), type(p2))
+    if isinstance(p1, utils.Params):
+      self.assertEqual(p1.type_, p2.type_)
+      tf.nest.map_structure(
+          lambda p1_, p2_: self.compare_params(p1_, p2_, skip_tensor_values),
+          p1.params, p2.params)
+    elif tf.is_tensor(p1):
+      if not skip_tensor_values:
+        self.assertAllEqual(p1, p2)
+    else:
+      self.assertEqual(p1, p2)
+
+  def testGetAndMakeFromParameters(self):
+    one = tf.constant(1.0)
+    d = tfp.distributions.Normal(
+        loc=one, scale=3.0, validate_args=True)
+    d = tfp.bijectors.Tanh()(d)
+    d = tfp.bijectors.Tanh()(d)
+    p = utils.get_parameters(d)
+
+    expected_p = utils.Params(
+        tfp.distributions.TransformedDistribution,
+        params={
+            'bijector':
+                utils.Params(
+                    tfp.bijectors.Chain,
+                    params={'bijectors': [
+                        utils.Params(tfp.bijectors.Tanh, params={}),
+                        utils.Params(tfp.bijectors.Tanh, params={}),
+                    ]}),
+            'distribution':
+                utils.Params(
+                    tfp.distributions.Normal,
+                    params={'validate_args': True, 'scale': 3.0, 'loc': one})})
+
+    self.compare_params(p, expected_p)
+
+    d_recreated = utils.make_from_parameters(p)
+    points = [0.01, 0.25, 0.5, 0.75, 0.99]
+    self.assertAllClose(d.log_prob(points), d_recreated.log_prob(points))
+
+  def testGetAndMakeNontrivialBijectorFromParameters(self):
+    scale_matrix = tf.Variable([[1.0, 2.0], [-1.0, 0.0]])
+    d = tfp.distributions.MultivariateNormalDiag(
+        loc=[1.0, 1.0], scale_diag=[2.0, 3.0], validate_args=True)
+    b = tfp.bijectors.ScaleMatvecLinearOperator(
+        scale=tf.linalg.LinearOperatorFullMatrix(matrix=scale_matrix),
+        adjoint=True)
+    b_d = b(d)
+    p = utils.get_parameters(b_d)
+
+    expected_p = utils.Params(
+        tfp.distributions.TransformedDistribution,
+        params={
+            'bijector': utils.Params(
+                tfp.bijectors.ScaleMatvecLinearOperator,
+                params={'adjoint': True,
+                        'scale': utils.Params(
+                            tf.linalg.LinearOperatorFullMatrix,
+                            params={'matrix': scale_matrix})}),
+            'distribution': utils.Params(
+                tfp.distributions.MultivariateNormalDiag,
+                params={'validate_args': True,
+                        'scale_diag': [2.0, 3.0],
+                        'loc': [1.0, 1.0]})})
+
+    self.compare_params(p, expected_p)
+
+    b_d_recreated = utils.make_from_parameters(p)
+
+    points = [[-1.0, -2.0],
+              [0.0, 0.0],
+              [3.0, -5.0],
+              [5.0, 5.0],
+              [1.0, np.inf],
+              [-np.inf, 0.0]]
+    self.assertAllClose(
+        b_d.log_prob(points), b_d_recreated.log_prob(points))
+
+  def testParametersToAndFromDict(self):
+    scale_matrix = tf.Variable([[1.0, 2.0], [-1.0, 0.0]])
+    d = tfp.distributions.MultivariateNormalDiag(
+        loc=[1.0, 1.0], scale_diag=[2.0, 3.0], validate_args=True)
+    b = tfp.bijectors.ScaleMatvecLinearOperator(
+        scale=tf.linalg.LinearOperatorFullMatrix(matrix=scale_matrix),
+        adjoint=True)
+    b_d = b(d)
+    p = utils.get_parameters(b_d)
+
+    p_dict = utils.parameters_to_dict(p)
+    expected_p_dict = {
+        'bijector': {'adjoint': True,
+                     'scale': {'matrix': scale_matrix}},
+        'distribution': {'validate_args': True,
+                         # These are deeply nested because we passed lists
+                         # intead of numpy arrays for `loc` and `scale_diag`.
+                         'scale_diag:0': 2.0,
+                         'scale_diag:1': 3.0,
+                         'loc:0': 1.0,
+                         'loc:1': 1.0}}
+
+    tf.nest.map_structure(
+        self.assertAllEqual, p_dict, expected_p_dict)
+
+    # This converts the tf.Variable entry in the matrix to a tf.Tensor
+    p_dict['bijector']['scale']['matrix'] = (
+        p_dict['bijector']['scale']['matrix'] + 1.0)
+
+    p_recreated = utils.merge_to_parameters_from_dict(
+        p, p_dict)
+
+    self.assertAllClose(
+        p_recreated.params['bijector'].params['scale'].params['matrix'],
+        p.params['bijector'].params['scale'].params['matrix'] + 1.0)
+
+    # Skip the tensor value comparison -- we checked it above.
+    self.compare_params(p, p_recreated, skip_tensor_values=True)
+
+  def testParametersFromDictMissingNestedParamsKeyFailure(self):
+    one = tf.constant(1.0)
+    d = tfp.distributions.Normal(
+        loc=one, scale=3.0, validate_args=True)
+    d = tfp.bijectors.Tanh()(d)
+    d = tfp.bijectors.Tanh()(d)
+    p = utils.get_parameters(d)
+    p_dict = utils.parameters_to_dict(p)
+
+    # Add a third bijector, changing the structure of the nest.
+    self.assertIn('bijectors:0', p_dict['bijector'].keys())
+    self.assertIn('bijectors:1', p_dict['bijector'].keys())
+    p_dict['bijector']['bijectors:2'] = p_dict['bijector']['bijectors:0']
+
+    # Flattening nested params lost information about the nested structure, so
+    # we can't e.g. add a new bijector in the dict and expect to put that back
+    # into the bijector list when converting back.
+    with self.assertRaisesRegex(
+        ValueError,
+        r'params_dict keys: \[\'bijectors:0\', \'bijectors:1\', '
+        r'\'bijectors:2\'\], value.params processed keys: '
+        r'\[\'bijectors:0\', \'bijectors:1\'\]'):
+      utils.merge_to_parameters_from_dict(p, p_dict)
+
+  def testParametersFromDictMissingNestedDictKeyFailure(self):
+    one = tf.constant(1.0)
+    d = tfp.distributions.Normal(
+        loc=one, scale=3.0, validate_args=True)
+    d = tfp.bijectors.Tanh()(d)
+    d = tfp.bijectors.Tanh()(d)
+    p = utils.get_parameters(d)
+    p_dict = utils.parameters_to_dict(p)
+
+    # Remove a non-nested key in the dictionary; this is fine.
+    del p_dict['distribution']['validate_args']
+
+    # We can reconstruct from this (we just use the default value from p)
+    utils.merge_to_parameters_from_dict(p, p_dict)
+
+    # Remove a nested entry in the dictionary; this can lead to subtle errors so
+    # we don't allow it.
+    del p_dict['bijector']['bijectors:1']
+
+    # Flattening nested params lost information about the nested structure, so
+    # we can't e.g. remove a bijector from a list and override just a subset of
+    # the nested bijectors list.
+    with self.assertRaisesRegex(
+        KeyError,
+        r'Missing a required nested element from params_dict.keys: '
+        r'\'bijectors:1\'.  params_dict.keys: \[\'bijectors:0\'\]'):
+      utils.merge_to_parameters_from_dict(p, p_dict)
 
 
 if __name__ == '__main__':
