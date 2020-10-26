Fix a bug in the mixture_policy that the info fields were gathered throught the wrong dimension.
PiperOrigin-RevId: 339010795
Change-Id: I4521436ac8e6c9b0d0005b13f5fe1ac266621481

tf_agents/bandits/policies/mixture_policy.py
@@ -117,8 +117,9 @@ class MixturePolicy(tf_policy.TFPolicy):
         lambda t: tf.gather(t, policy_choice, batch_dims=1), policy_actions)
 
     expanded_mixture_info = tf.nest.map_structure(
-        lambda t: tf.gather(t, expanded_choice, batch_dims=1), policy_infos)
-    mixture_info = tf.nest.map_structure(lambda t: tf.squeeze(t, axis=1),
+        lambda t: tf.gather(t, expanded_choice, batch_dims=1, axis=-1),
+        policy_infos)
+    mixture_info = tf.nest.map_structure(lambda t: tf.squeeze(t, axis=-1),
                                          expanded_mixture_info)
     return policy_step.PolicyStep(mixture_action, policy_state, {
         MIXTURE_AGENT_ID: policy_choice,

tf_agents/bandits/policies/mixture_policy_test.py
@@ -42,7 +42,8 @@ class ConstantPolicy(tf_policy.TFPolicy):
     super(ConstantPolicy, self).__init__(
         time_step_spec=time_step_spec,
         action_spec=action_spec,
-        info_spec={'a': tensor_spec.TensorSpec(shape=(), dtype=tf.int32)})
+        info_spec={'a': tensor_spec.TensorSpec(shape=(), dtype=tf.int32),
+                   'b': tensor_spec.TensorSpec(shape=(4,), dtype=tf.float32)})
 
   def _variables(self):
     return []
@@ -55,7 +56,8 @@ class ConstantPolicy(tf_policy.TFPolicy):
     batch_size = tf.compat.dimension_value(tf.shape(time_step.observation)[0])
     return policy_step.PolicyStep(
         tf.fill([batch_size], self._constant_action), policy_state,
-        {'a': tf.range(batch_size, dtype=tf.int32)})
+        {'a': tf.range(batch_size, dtype=tf.int32),
+         'b': tf.ones(shape=[batch_size, 4], dtype=tf.float32)})
 
 
 class MixturePolicyTest(test_utils.TestCase):