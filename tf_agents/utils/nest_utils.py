[TF-Agents] Fix StreamingTensorNormalizer variance calculation.
While we're at it, remove some old methods that no one uses.

Fixes #530.

PiperOrigin-RevId: 366286314
Change-Id: Iea6c76a2fdc7dff83487448126b34dd3efe412cd

tf_agents/utils/nest_utils.py
@@ -782,7 +782,19 @@ def flatten_multi_batched_nested_tensors(tensors, specs):
 
 
 def get_outer_shape(nested_tensor, spec):
-  """Runtime batch dims of tensor's batch dimension `dim`."""
+  """Runtime batch dims of tensor's batch dimension `dim`.
+
+  Args:
+    nested_tensor: Nest of tensors.
+    spec: The nested spec.
+
+  Returns:
+    A `Tensor` containing the outer shape.
+
+  Raises:
+    ValueError: If `nested_tensor` and `spec` have different structures.
+    TypeError: If `nested_tensor` and `spec` structures have differing types.
+  """
   assert_same_structure(
       nested_tensor,
       spec,
@@ -794,7 +806,7 @@ def get_outer_shape(nested_tensor, spec):
   num_outer_dims = (len(first_tensor.shape) - len(first_spec.shape))
   if not is_batched_nested_tensors(
       nested_tensor, spec, num_outer_dims=num_outer_dims):
-    return []
+    return tf.constant([], dtype=tf.int32)
 
   return tf.shape(input=first_tensor)[:num_outer_dims]
 

tf_agents/utils/nest_utils_test.py
@@ -134,7 +134,7 @@ class NestedTensorsTest(tf.test.TestCase):
     tensor = tf.zeros([2, 3], dtype=tf.float32)
     spec = tensor_spec.TensorSpec([2, 3], dtype=tf.float32)
     batch_size = nest_utils.get_outer_shape(tensor, spec)
-    self.assertEqual(self.evaluate(batch_size), [])
+    self.assertAllEqual(self.evaluate(batch_size), [])
 
   def testGetOuterShapeOneDim(self):
     tensor = tf.zeros([5, 2, 3], dtype=tf.float32)
@@ -158,7 +158,7 @@ class NestedTensorsTest(tf.test.TestCase):
     spec = tensor_spec.TensorSpec([None, 1], dtype=tf.float32)
     tensor = tf.convert_to_tensor(value=[[0.0]] * 8)
     batch_size = self.evaluate(nest_utils.get_outer_shape(tensor, spec))
-    self.assertEqual(batch_size, [])
+    self.assertAllEqual(batch_size, [])
 
   def testGetOuterDimsSingleTensorUnbatched(self):
     tensor = tf.zeros([2, 3], dtype=tf.float32)

tf_agents/utils/tensor_normalizer.py
@@ -57,15 +57,6 @@ class TensorNormalizer(tf.Module):
     self._flat_tensor_spec = tf.nest.flatten(tensor_spec)
     self._create_variables()
 
-  @property
-  def nested(self):
-    """True if tensor is nested, False otherwise."""
-    return tf.nest.is_nested(self._tensor_spec)
-
-  @abc.abstractmethod
-  def copy(self, scope=None):
-    """Copy constructor for TensorNormalizer."""
-
   @abc.abstractmethod
   def _create_variables(self):
     """Uses self._scope and creates all variables needed for the normalizer."""
@@ -87,7 +78,7 @@ class TensorNormalizer(tf.Module):
 
   @abc.abstractmethod
   def _get_mean_var_estimates(self):
-    """Returns this normalizer's current estimates for mean & variance."""
+    """Returns this normalizer's current estimates for mean & var (flat)."""
 
   def update(self, tensor, outer_dims=(0,)):
     """Updates tensor normalizer variables."""
@@ -111,7 +102,9 @@ class TensorNormalizer(tf.Module):
     Returns:
       normalized_tensor: Tensor after applying normalization.
     """
-    nest_utils.assert_same_structure(tensor, self._tensor_spec)
+    nest_utils.assert_matching_dtypes_and_inner_shapes(
+        tensor, self._tensor_spec, caller=self,
+        tensors_name='tensors', specs_name='tensor_spec')
     tensor = tf.nest.flatten(tensor)
     tensor = tf.nest.map_structure(lambda t: tf.cast(t, tf.float32), tensor)
 
@@ -162,12 +155,6 @@ class EMATensorNormalizer(TensorNormalizer):
       norm_update_rate=0.001):
     super(EMATensorNormalizer, self).__init__(tensor_spec, scope)
     self._norm_update_rate = norm_update_rate
-
-  def copy(self, scope=None):
-    """Copy constructor for EMATensorNormalizer."""
-    scope = scope if scope is not None else self._scope
-    return EMATensorNormalizer(
-        self._tensor_spec, scope=scope, norm_update_rate=self._norm_update_rate)
 
   def _create_variables(self):
     """Creates the variables needed for EMATensorNormalizer."""
@@ -238,107 +225,99 @@ class StreamingTensorNormalizer(TensorNormalizer):
   """Normalizes mean & variance based on full history of tensor values."""
 
   def _create_variables(self):
-    """Uses self._scope and creates all variables needed for the normalizer."""
-    self._count = tf.nest.map_structure(
-        lambda spec: create_variable('count', 1e-8, spec.shape, tf.float32),
-        self._flat_tensor_spec)
-    self._mean_sum = tf.nest.map_structure(
-        lambda spec: create_variable('mean_sum', 0, spec.shape, tf.float32),
-        self._flat_tensor_spec)
-    self._var_sum = tf.nest.map_structure(
-        lambda spec: create_variable('var_sum', 0, spec.shape, tf.float32),
-        self._flat_tensor_spec)
-
-  def copy(self, scope=None):
-    """Copy constructor for StreamingTensorNormalizer."""
-    scope = scope if scope is not None else self._scope
-    return StreamingTensorNormalizer(self._tensor_spec, scope=scope)
+    """Create all variables needed for the normalizer."""
+    self._count = [
+        create_variable('count_%d' % i, 1e-8, spec.shape, tf.float32,
+                        trainable=False)
+        for i, spec in enumerate(self._flat_tensor_spec)
+    ]
+    self._avg = [
+        create_variable('avg_%d' % i, 0, spec.shape, tf.float32,
+                        trainable=False)
+        for i, spec in enumerate(self._flat_tensor_spec)
+    ]
+    self._m2 = [
+        create_variable('m2_%d' % i, 0, spec.shape, tf.float32,
+                        trainable=False)
+        for i, spec in enumerate(self._flat_tensor_spec)
+    ]
+    # var is just m2 / count, but we include it for efficiency and to
+    # force a hard break for old checkpoints.
+    self._var = [
+        create_variable('var_%d' % i, 0, spec.shape, tf.float32,
+                        trainable=False)
+        for i, spec in enumerate(self._flat_tensor_spec)
+    ]
 
   @property
   def variables(self):
-    """Returns a tuple of tf variables owned by this normalizer."""
+    """Returns a tuple of nested TF Variables owned by this normalizer."""
     return (tf.nest.pack_sequence_as(self._tensor_spec, self._count),
-            tf.nest.pack_sequence_as(self._tensor_spec, self._mean_sum),
-            tf.nest.pack_sequence_as(self._tensor_spec, self._var_sum))
+            tf.nest.pack_sequence_as(self._tensor_spec, self._avg),
+            tf.nest.pack_sequence_as(self._tensor_spec, self._m2),
+            tf.nest.pack_sequence_as(self._tensor_spec, self._var))
 
-  def _update_ops(self, tensor, outer_dims):
+  def _update_ops(self, tensors, outer_dims):
     """Returns a list of ops which update normalizer variables for tensor.
 
-    This normalizer computes the absolute mean of all observed tensor values,
-    and keeps a biased estimator of variance, by summing all observed mean and
-    variance values and dividing the sum by the count of samples seen.
-
     Args:
-      tensor: The tensor of values to be normalized.
-      outer_dims: The batch dimensions over which to compute normalization
-        statistics.
+      tensors: The tensors of values to be normalized.
+      outer_dims: Ignored.  The batch dimensions are extracted by comparing
+        the associated tensor with the specs.
 
     Returns:
       A list of ops, which when run will update all necessary normaliztion
       variables.
     """
+    del outer_dims
 
-    def _tensor_update_ops(single_tensor, single_mean_est, count_var, mean_var,
-                           var_var):
-      """Make update ops for a single non-nested tensor."""
-      # Num samples in batch is the product of batch dimensions.
-      num_samples = tf.cast(
-          tf.reduce_prod(
-              input_tensor=tf.gather(tf.shape(
-                  input=single_tensor), outer_dims)), tf.float32)
-      mean_sum = tf.reduce_sum(input_tensor=single_tensor, axis=outer_dims)
-      var_sum = tf.reduce_sum(
-          input_tensor=tf.square(single_tensor - single_mean_est),
-          axis=outer_dims)
+    nest_utils.assert_matching_dtypes_and_inner_shapes(
+        tensors, self._tensor_spec, caller=self,
+        tensors_name='tensors', specs_name='tensor_spec')
+    outer_shape = nest_utils.get_outer_shape(tensors, self._tensor_spec)
+    outer_rank_static = tf.compat.dimension_value(outer_shape.shape[0])
+    outer_axes = (
+        list(range(outer_rank_static)) if outer_rank_static is not None else
+        tf.range(tf.size(outer_shape)))
 
-      # Ops to update streaming norm. Make sure that all stats are computed
-      #   before updates are performed.
-      with tf.control_dependencies([num_samples, mean_sum, var_sum]):
-        update_ops = [
-            tf.compat.v1.assign_add(
-                count_var,
-                tf.ones_like(count_var) * num_samples,
-                name='update_count'),
-            tf.compat.v1.assign_add(mean_var, mean_sum, name='update_mean_sum'),
-            tf.compat.v1.assign_add(var_var, var_sum, name='update_var_sum'),
-        ]
-      return update_ops
+    n_a = tf.cast(tf.reduce_prod(outer_shape), tf.float32)
 
-    mean_estimate, _ = self._get_mean_var_estimates()
+    flat_tensors = tf.nest.flatten(tensors)
 
-    tensor = tf.nest.flatten(tensor)
-    # Aggregate update ops for all parts of potentially nested tensor.
-    updates = tf.nest.map_structure(_tensor_update_ops, tensor, mean_estimate,
-                                    self._count, self._mean_sum, self._var_sum)
-    all_update_ops = tf.nest.flatten(updates)
+    update_ops = []
 
-    return all_update_ops
+    for i, t in enumerate(flat_tensors):
+      avg_a, var_a = tf.nn.moments(t, axes=outer_axes)
+      m2_a = var_a * n_a
+      n_b = self._count[i]
+      avg_b = self._avg[i]
+      m2_b = self._m2[i]
+      n_ab, avg_ab, m2_ab = parallel_variance_calculation(
+          n_a, avg_a, m2_a, n_b, avg_b, m2_b)
+      with tf.control_dependencies([n_ab, avg_ab, m2_ab]):
+        update_ops.extend([
+            self._count[i].assign(n_ab),
+            self._avg[i].assign(avg_ab),
+            self._m2[i].assign(m2_ab),
+            self._var[i].assign(m2_ab / n_ab)
+        ])
+
+    return update_ops
 
   def _get_mean_var_estimates(self):
-    """Returns this normalizer's current estimates for mean & variance."""
-    mean_estimate = nest_utils.map_structure_up_to(
-        self._flat_tensor_spec,
-        lambda a, b: a / b, self._mean_sum,
-        self._count)
-    var_estimate = nest_utils.map_structure_up_to(
-        self._flat_tensor_spec,
-        lambda a, b: a / b, self._var_sum,
-        self._count)
-    return mean_estimate, var_estimate
+    """Returns this normalizer's current estimates for mean & var (flat)."""
+    return (self._avg, self._var)
 
   def reset(self):
     """Reset the count, mean and variance to its initial state."""
-    reset_ops = [
-        tf.nest.map_structure(
-            lambda var: var.assign(tf.constant(1e-8, shape=var.shape)),
-            self._count),
-        tf.nest.map_structure(
-            lambda var: var.assign(tf.constant(0., shape=var.shape)),
-            self._mean_sum),
-        tf.nest.map_structure(
-            lambda var: var.assign(tf.constant(0., shape=var.shape)),
-            self._var_sum),
-    ]
+    reset_ops = []
+    for i in range(len(self._count)):
+      reset_ops.extend([
+          self._count[i].assign(1e-8*tf.ones_like(self._count[i])),
+          self._avg[i].assign(tf.zeros_like(self._avg[i])),
+          self._m2[i].assign(tf.zeros_like(self._m2[i])),
+          self._var[i].assign(tf.zeros_like(self._var[i])),
+      ])
 
     return reset_ops
 

tf_agents/utils/tensor_normalizer_test.py
@@ -19,6 +19,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import functools
+
 from absl.testing import parameterized
 import numpy as np
 import tensorflow as tf
@@ -207,165 +209,92 @@ class StreamingTensorNormalizerTest(tf.test.TestCase, parameterized.TestCase):
     self.evaluate(tf.compat.v1.global_variables_initializer())
 
   def testGetVariables(self):
-    count_var, means_var, variances_var = (self._tensor_normalizer.variables)
+    count_var, avg_var, m2_var, var_var = (self._tensor_normalizer.variables)
     self.assertAllEqual(count_var.shape, self._tensor_spec.shape)
-    self.assertAllEqual(means_var.shape, self._tensor_spec.shape)
-    self.assertAllEqual(variances_var.shape, self._tensor_spec.shape)
-
-  def testUpdateVariables(self):
-    # Get original mean and variance.
-    original_count, original_mean_sum, original_variance_sum = self.evaluate(
-        self._tensor_normalizer.variables)
-
-    # Construct and evaluate normalized tensor. Should update mean &
-    #   variance.
-    np_array = np.array([[1.3, 4.2, 7.5],
-                         [8.3, 2.2, 9.5],
-                         [3.3, 5.2, 6.5]], np.float32)
-    tensor = tf.constant(np_array, dtype=tf.float32)
-    update_norm_vars = self._tensor_normalizer.update(tensor)
-    self.evaluate(update_norm_vars)
-
-    # Get new mean and variance, and make sure they changed.
-    new_count, new_mean_sum, new_variance_sum = self.evaluate(
-        self._tensor_normalizer.variables)
-
-    self.assertAllEqual(new_count,
-                        np.array([3, 3, 3], dtype=np.float32) + original_count)
-    self.assertAllClose(new_mean_sum,
-                        np.sum(np_array, axis=0) + original_mean_sum)
-    self.assertAllClose(
-        new_variance_sum,
-        np.sum(np.square(np_array - original_mean_sum), axis=0) +
-        original_variance_sum)
+    self.assertAllEqual(avg_var.shape, self._tensor_spec.shape)
+    self.assertAllEqual(m2_var.shape, self._tensor_spec.shape)
+    self.assertAllEqual(var_var.shape, self._tensor_spec.shape)
 
   def testReset(self):
     # Get original mean and variance.
-    original_count, original_mean_sum, original_variance_sum = self.evaluate(
-        self._tensor_normalizer.variables)
+    original_vars = self.evaluate(self._tensor_normalizer.variables)
 
-    # Construct and evaluate normalized tensor. Should update mean &
-    #   variance.
+    # Construct and evaluate normalized tensor. Updates statistics.
     np_array = np.array([[1.3, 4.2, 7.5], [8.3, 2.2, 9.5], [3.3, 5.2, 6.5]],
                         np.float32)
     tensor = tf.constant(np_array, dtype=tf.float32)
     update_norm_vars = self._tensor_normalizer.update(tensor)
     self.evaluate(update_norm_vars)
 
-    # Get new mean and variance, and make sure they changed.
-    new_count, new_mean_sum, new_variance_sum = self.evaluate(
-        self._tensor_normalizer.variables)
-
-    self.assertAllEqual(new_count,
-                        np.array([3, 3, 3], dtype=np.float32) + original_count)
-    self.assertAllClose(new_mean_sum,
-                        np.sum(np_array, axis=0) + original_mean_sum)
-    self.assertAllClose(
-        new_variance_sum,
-        np.sum(np.square(np_array - original_mean_sum), axis=0) +
-        original_variance_sum)
-
-    # Verify that the count, mean and variance have been successfully reset.
+    # Verify that the internal variables have been successfully reset.
     self.evaluate(self._tensor_normalizer.reset())
+    reset_vars = self.evaluate(self._tensor_normalizer.variables)
+    self.assertAllClose(original_vars, reset_vars)
 
-    new_count, new_mean_sum, new_variance_sum = self.evaluate(
-        self._tensor_normalizer.variables)
-    self.assertAllEqual(new_count, original_count)
-    self.assertAllClose(new_mean_sum, original_mean_sum)
-    self.assertAllClose(new_variance_sum, original_variance_sum)
-
-  def testUpdateVariablesDictNest(self):
-    # Get original mean and variance.
-    original_count, original_mean_sum, original_variance_sum = self.evaluate(
-        self._dict_tensor_normalizer.variables)
-
+  def testUpdate(self):
     # Construct and evaluate normalized tensor. Should update mean &
     #   variance.
     np_array = np.array([[1.3, 4.2, 7.5],
                          [8.3, 2.2, 9.5],
                          [3.3, 5.2, 6.5]], np.float32)
-    tensor = {'a': tf.constant(np_array, dtype=tf.float32),
-              'b': tf.constant(np_array, dtype=tf.float32)}
-    update_norm_vars = self._dict_tensor_normalizer.update(tensor)
+    tensors = {'a': tf.constant(np_array, dtype=tf.float32),
+               'b': tf.constant(np_array, dtype=tf.float32)}
+
+    def _compare(data):
+      n = data.shape[0]
+      expected_avg = data.mean(axis=0)
+      expected_var = data.var(axis=0)
+      expected_m2 = expected_var * n
+      expected_count = np.array([n] * 3)
+
+      new_count, new_avg, new_m2, new_var = self.evaluate(
+          self._dict_tensor_normalizer.variables)
+
+      tf.nest.map_structure(lambda v: self.assertAllClose(v, expected_count),
+                            new_count)
+      tf.nest.map_structure(lambda v: self.assertAllClose(v, expected_avg),
+                            new_avg)
+      tf.nest.map_structure(lambda v: self.assertAllClose(v, expected_var),
+                            new_var)
+      tf.nest.map_structure(lambda v: self.assertAllClose(v, expected_m2),
+                            new_m2)
+
+    update_norm_vars = self._dict_tensor_normalizer.update(tensors)
     self.evaluate(update_norm_vars)
+    _compare(data=np_array)
 
-    # Get new mean and variance, and make sure they changed.
-    new_count, new_mean_sum, new_variance_sum = self.evaluate(
-        self._dict_tensor_normalizer.variables)
+    update_norm_vars = self._dict_tensor_normalizer.update(
+        tf.nest.map_structure(lambda t: t + 1.0, tensors))
+    self.evaluate(update_norm_vars)
+    _compare(data=np.concatenate((np_array, np_array + 1.0), axis=0))
 
-    expected_count = {k: (np.array([3, 3, 3], dtype=np.float32) +
-                          original_count[k]) for k in original_count}
-    expected_mean_sum = {k: (np.sum(np_array, axis=0) +
-                             original_mean_sum[k]) for k in original_mean_sum}
-    expected_variance_sum = {
-        k: (np.sum(np.square(np_array - original_mean_sum[k]), axis=0) +
-            original_variance_sum[k]) for k in original_variance_sum}
+    update_norm_vars = self._dict_tensor_normalizer.update(
+        tf.nest.map_structure(lambda t: t - 1.0, tensors))
+    self.evaluate(update_norm_vars)
+    _compare(
+        data=np.concatenate((np_array, np_array + 1.0, np_array - 1.0), axis=0))
 
-    def _assert_dicts_close(dict1, dict2):
-      self.assertAllEqual(sorted(dict1.keys()), sorted(dict2.keys()))
-      self.assertAllClose([dict1[k] for k in dict1.keys()],
-                          [dict2[k] for k in dict1.keys()])
+  def testNormalization(self):
+    as_tensor = functools.partial(tf.convert_to_tensor, dtype=tf.float32)
+    norm_obs = {'a': np.random.randn(6, 2, 3),
+                'b': np.random.randn(6, 2, 3)}
+    norm_obs_t = tf.nest.map_structure(as_tensor, norm_obs)
+    view_obs = {'a': np.random.randn(4, 3),
+                'b': np.random.randn(4, 3)}
+    view_obs_t = tf.nest.map_structure(as_tensor, view_obs)
+    self.evaluate(self._dict_tensor_normalizer.update(norm_obs_t))
+    observed = self.evaluate(
+        self._dict_tensor_normalizer.normalize(
+            view_obs_t, clip_value=-1, variance_epsilon=1e-6))
+    norm_obs_avg = tf.nest.map_structure(
+        lambda a: a.mean(axis=(0, 1)), norm_obs)
+    norm_obs_std = tf.nest.map_structure(
+        lambda a: a.std(axis=(0, 1)), norm_obs)
+    expected = tf.nest.map_structure(
+        lambda obs, avg, std: (obs - avg)/std,
+        view_obs, norm_obs_avg, norm_obs_std)
+    self.assertAllClose(observed, expected)
 
-    _assert_dicts_close(new_count, expected_count)
-    _assert_dicts_close(new_mean_sum, expected_mean_sum)
-    _assert_dicts_close(new_variance_sum, expected_variance_sum)
-
-  @parameterized.named_parameters(
-      ('OneReduceAxis', 1),
-      ('TwoReduceAxes', 2),
-  )
-  def testNormalization(self, num_outer_dims):
-    count_var, means_var, variance_var = self._tensor_normalizer.variables
-    self.evaluate([
-        tf.compat.v1.assign(count_var, [1.0] * 3),
-        tf.compat.v1.assign(means_var, [10.0] * 3),
-        tf.compat.v1.assign(variance_var, [0.1] * 3)
-    ])
-
-    vector = [9.0, 10.0, 11.0]
-    # Above, the estimated mean was set to 10, and variance to 0.1. Thus the
-    # estimated stddev is sqrt(0.1) = 0.3162.
-    # The middle sample falls on the mean, so should be normalized to 0.0. Each
-    # of the other samples is 1 away from the mean. 1 / 0.3162 = 3.162
-    expected = [-3.1622776601, 0.0, 3.1622776601]
-    for _ in range(num_outer_dims - 1):
-      vector = [vector] * 2
-      expected = [expected] * 2
-    tensor = tf.constant(vector)
-
-    norm_obs = self._tensor_normalizer.normalize(
-        tensor, variance_epsilon=0.0)
-    self.assertAllClose(expected, self.evaluate(norm_obs), atol=0.0001)
-
-  def testNormalizationDictNest(self):
-    count_var, means_var, variance_var = self._dict_tensor_normalizer.variables
-    self.evaluate(  # For each var in nest, assign initial value.
-        [tf.compat.v1.assign(var, [1.0] * 3) for var in count_var.values()] +
-        [tf.compat.v1.assign(var, [10.0] * 3) for var in means_var.values()] +
-        [tf.compat.v1.assign(var, [.1] * 3) for var in variance_var.values()])
-
-    vector = [9.0, 10.0, 11.0]
-    expected = {'a': [-3.1622776601, 0.0, 3.1622776601],
-                'b': [-3.1622776601, 0.0, 3.1622776601]}
-    tensor = {'a': tf.constant(vector), 'b': tf.constant(vector)}
-
-    norm_obs = self._dict_tensor_normalizer.normalize(
-        tensor, variance_epsilon=0.0)
-    self.assertAllClose(expected, self.evaluate(norm_obs), atol=0.0001)
-
-  def testShouldNotCenterMean(self):
-    count_var, means_var, variance_var = self._tensor_normalizer.variables
-    self.evaluate([
-        tf.compat.v1.assign(count_var, [1.0] * 3),
-        tf.compat.v1.assign(means_var, [10.0] * 3),
-        tf.compat.v1.assign(variance_var, [0.01] * 3)
-    ])
-    tensor = tf.constant([[9.0, 10.0, 11.0]])
-    norm_obs = self._tensor_normalizer.normalize(
-        tensor, center_mean=False,
-        variance_epsilon=0.0, clip_value=0.0)
-    expected = [[90.0, 100.0, 110.0]]
-    self.assertAllClose(expected, self.evaluate(norm_obs))
 
 if __name__ == '__main__':
   tf.test.main()