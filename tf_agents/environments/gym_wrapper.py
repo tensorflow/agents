e3770d12c2ee973799b1916a7592be413043f72c
Cyril MARLIN <cyril.marlin@gmail.com>
2021-06-04 14:47:19 +0200

    fix indentation

--- tf_agents/environments/gym_wrapper.py
+++ tf_agents/environments/gym_wrapper.py
@@ -165,8 +165,11 @@ class GymWrapper(py_environment.PyEnvironment):
     self._action_spec = spec_from_gym_space(self._gym_env.action_space,
                                             spec_dtype_map, simplify_box_bounds,
                                             'action')
-    self._reward_spec = specs.BoundedArraySpec(
-        shape=(), dtype=np.float64, minimum=gym_env.reward_range[0], maximum=gym_env.reward_range[1], name='reward')
+    self._reward_spec = specs.BoundedArraySpec(shape=(),
+                                              dtype=np.float64,
+                                              minimum=gym_env.reward_range[0],
+                                              maximum=gym_env.reward_range[1],
+                                              name='reward')
     self._flat_obs_spec = tf.nest.flatten(self._observation_spec)
     self._render_kwargs = render_kwargs or {}
     self._info = None
