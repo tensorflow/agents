934ea2fdf5bb2cb319b7961b5ca1af25f9e5176d
Yu Feng <feyu@google.com>
2021-08-18 14:12:55 -0700

    Fix broken formatting of document.

--- tf_agents/utils/eager_utils.py
+++ tf_agents/utils/eager_utils.py
@@ -152,7 +152,8 @@ def future_in_eager_mode(func_or_method):
     loss = loss_fn(inputs)
     # Now loss is a Future callable.
     loss_value = loss()
-
+  ```
+  
   Args:
     func_or_method: A function or method to decorate.
 
