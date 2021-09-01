Fix bug in PPO advantage normalization.
Normalize across both the batch and time dimension. Standard setups using PPOLearner were not impacted by this bug. However, this could cause issues with custom data pipelines. Originally discovered in https://github.com/tensorflow/agents/issues/651 by cedavidyang (Thank you for spotting this issue!).
Fixes https://github.com/tensorflow/agents/issues/651

PiperOrigin-RevId: 394328482
Change-Id: If5055f0be421fdbe1fbe26284a33bab6822e4744

tf_agents/agents/ppo/ppo_agent.py
@@ -98,7 +98,7 @@ PPOLossInfo = collections.namedtuple('PPOLossInfo', (
 ))
 
 
-def _normalize_advantages(advantages, axes=(0,), variance_epsilon=1e-8):
+def _normalize_advantages(advantages, axes=(0, 1), variance_epsilon=1e-8):
   adv_mean, adv_var = tf.nn.moments(advantages, axes=axes, keepdims=True)
   normalized_advantages = tf.nn.batch_normalization(
       advantages,