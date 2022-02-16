b81808fb58c0b863d9a20ae3b6e85228c5068469
Yao Lu <yaolug@google.com>
2022-02-16 09:39:37 -0800

    Remove DeterministicWithLogProb in greedy_policy since the bug has been resolved. This will remove the need to import this policy when using a saved_model.
    PiperOrigin-RevId: 429069941
    Change-Id: Iff7889ca86c99fcc86327f295bf78723514bd4d3

--- tf_agents/policies/greedy_policy.py
+++ tf_agents/policies/greedy_policy.py
@@ -28,16 +28,6 @@ from tf_agents.policies import tf_policy
 from tf_agents.trajectories import policy_step
 
 
-# TODO(b/131405384): Remove this once Deterministic does casting internally.
-@tfp.experimental.register_composite
-class DeterministicWithLogProb(tfp.distributions.Deterministic):
-  """Thin wrapper around Deterministic that supports taking log_prob."""
-
-  def _log_prob(self, x):
-    """Takes log-probs by casting to tf.float32 instead of self.dtype."""
-    return tf.math.log(tf.cast(self.prob(x), dtype=tf.float32))
-
-
 @gin.configurable(module='tf_agents', denylist=['policy'])
 class GreedyPolicy(tf_policy.TFPolicy):
   """Returns greedy samples of a given policy."""
@@ -72,9 +62,10 @@ class GreedyPolicy(tf_policy.TFPolicy):
         greedy_action = dist.mode()
       except NotImplementedError:
         raise ValueError("Your network's distribution does not implement mode "
-                         "making it incompatible with a greedy policy.")
+                         'making it incompatible with a greedy policy.'
+                        ) from NotImplementedError
 
-      return DeterministicWithLogProb(loc=greedy_action)
+      return tfp.distributions.Deterministic(loc=greedy_action)
 
     distribution_step = self._wrapped_policy.distribution(
         time_step, policy_state)
