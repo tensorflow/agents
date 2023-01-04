fce066cdee99e8576b4af06fcc75336527b3e938
Toby Boyd <tobyboyd@google.com>
2023-01-04 10:17:21 -0800

    r/np.bool/bool and require 'numpy >= 1.19.0'
    numpy has deprecated np.bool and you are supposed to use `np._bool` or just python `bool`. We are using python bool, which was part of an internal change.
    
    PiperOrigin-RevId: 499517794
    Change-Id: I31336bcf25ad48f4c8582b0d3b084d98267a282c

--- setup.py
+++ setup.py
@@ -176,7 +176,7 @@ def get_required_packages():
       'cloudpickle >= 1.3',
       'gin-config >= 0.4.0',
       'gym >= 0.17.0, <=0.23.0',
-      'numpy >= 1.13.3',
+      'numpy >= 1.19.0',
       'pillow',
       'six >= 1.10.0',
       'protobuf >= 3.11.3',
